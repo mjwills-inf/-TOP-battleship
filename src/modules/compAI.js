c6584353827f9cd6dd635d56e4fc8061689744b0
mjwills-inf <mjwills@live.co.uk>
2020-06-30 18:35:09 +0100

    in compAI

--- src/modules/compAI.js
+++ src/modules/compAI.js
@@ -1,42 +1,57 @@
 const CompAI = (gameboard) => {
   let x; let y; let lastShotTile; let targetShip;
-  // possibly let target shots = 0
+  let targetShotCount = 0;
 
   const tiles = gameboard.getTilesArray();
 
   const shotOptions = [...tiles];
 
-  // const occupiedTiles = tiles.filter((element) => element.occupied === true);
-
-  // const hitOccupiedTiles = occupiedTiles.filter((element) => element.firedAt === true);
-
   const checkSurroundingTiles = (tile) => {
-
     // so we are here . targetShip set (not sunk), get availible tiles around ship
     // if only one shot on ship,random pick from surrounding
     // if 2 shots on ship, then in same axis
-  
-    console.log('checking surrounds tiles');
+    console.log('CHECKING SURROUNDING TILES');
     const refX = tile.x;
     const refY = tile.y;
-    let tileAbove; let tileBelow; let tileLeft; let tileRight;
+    const tileAbove = ((refX - 1) >= 1) ? tiles.find((ele) => ele.x === (refX - 1)
+        && ele.y === refY) : null;
+    const tileBelow = ((refX + 1) <= 10) ? tiles.find((ele) => ele.x === (refX + 1)
+        && ele.y === refY) : null;
+    const tileLeft = ((refY - 1) >= 1) ? tiles.find((ele) => ele.x === refX
+        && ele.y === (refY - 1)) : null;
+    const tileRight = ((refY + 1) <= 10) ? tiles.find((ele) => ele.x === refX
+        && ele.y === (refY + 1)) : null;
+
+    console.log('checkSurroundingTiles -> tileAbove', tileAbove);
+    console.log('checkSurroundingTiles -> tileBelow', tileBelow);
+    console.log('checkSurroundingTiles -> tileLeft', tileLeft);
+    console.log('checkSurroundingTiles -> tileRight', tileRight);
+
+    console.log('CompAI -> targetShotCount', targetShotCount);
+
+    // shotOptions.splice(chosenIndex, 1);
+    targetShotCount += 1;
   };
 
   function getRandomCoords() {
-    console.log('getting random coords');
+    console.log('getRandomCoords =>', targetShip);
     const randomIndex = Math.floor(Math.random() * (shotOptions.length + 1));
     const targetTile = shotOptions[randomIndex];
     x = targetTile.x;
     y = targetTile.y;
     lastShotTile = targetTile;
+    targetShotCount = 1;
     shotOptions.splice(randomIndex, 1);
   }
 
   const checkShipSunk = () => {
-    console.log('checking last shot ship has been sunk');
+    console.log('CHECKING SUNK');
     const shipName = lastShotTile.shipNameRef;
     const ship = gameboard.getFleet().find((ele) => ele.getName() === shipName);
+    console.log('check ship sunk = ', ship);
     if (ship.getSunk() === true) {
+      console.log(' if (ship.getSunk() === true) >>>>> call random coords');
+      targetShip = undefined;
       getRandomCoords();
     } else {
       targetShip = ship;
@@ -46,11 +61,15 @@ const CompAI = (gameboard) => {
 
 
   const chooseTarget = function chooseTarget() {
-    if (lastShotTile === undefined || lastShotTile.occupied === false) {
+    console.log('CHOOSING TARGET');
+    console.log('last shot tile = ', lastShotTile);
+    console.log('target ship =', targetShip);
+    if (lastShotTile === undefined) {
       getRandomCoords();
-    }
-    if (lastShotTile.occupied === true) {
+    } else if (lastShotTile.occupied === true) {
       checkShipSunk();
+    } else {
+      getRandomCoords();
     }
     return {
       x,
@@ -69,32 +88,6 @@ export default CompAI;
 //   tileAbove = tiles.find((ele) => ele.x === (refX - 1) && ele.y === refY);
 // }
 
-// if (tileAbove === undefined) {
-//   // shot is on edge of square (ship is not sunk)
-// }
-
-// if ((refX + 1) <= 10) {
-//   tileBelow = tiles.find((ele) => ele.x === (refX + 1) && ele.y === refY);
-// }
-
-// if (tileBelow === undefined) {
-//   // shot is on edge of square (ship is not sunk)
-// }
-
-// if ((refY - 1) >= 1) {
-//   tileLeft = tiles.find((ele) => ele.x === refX && ele.y === (refY - 1));
-// }
-
-// if (tileLeft === undefined) {
-//   // shot is on edge of square (ship is not sunk)
-// }
-// if ((refY + 1) <= 10) {
-//   tileRight = tiles.find((ele) => ele.x === refX && ele.y === (refY + 1));
-// }
-
-// if (tileRight === undefined) {
-//   // shot is on edge of square (ship is not sunk)
-// }
 
 // const compStupid = (gameboard) => {
 //   let x; let y;
