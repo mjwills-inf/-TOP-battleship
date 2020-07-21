cae3f02f96f00f3ee248b42c897badd8a2431b72
mjwills-inf <mjwills@live.co.uk>
2020-07-21 17:20:43 +0100

    start highlight

--- src/modules/dom_stuff/highlight.js
+++ src/modules/dom_stuff/highlight.js
@@ -1,13 +1,56 @@
+const change = (tile) => {
+  console.log('change THIS =>', tile);
+};
+
+console.log(change);
+
 const highlight = (eve) => {
   const shipRef = eve.target.getAttribute('data-ship-ref');
   const tileCoord = eve.target.getAttribute('data-xy-ref').split(',');
   const x = tileCoord[0];
   const y = tileCoord[1];
+  const upTiles = [];
+  const downTiles = [];
+  const leftTiles = [];
+  const rightTiles = [];
+
   if (shipRef === 'null') {
-    console.log('tile empty', x, y);
+    // console.log('tile empty', x, y);
 
     for (let i = Number(x); i <= 10; i += 1) {
-      console.log('this direction');
+      // console.log('down', i, y);
+      const targetTile = document.querySelector(`.js-data-xy-${i}-${y}`);
+      // console.log('down', targetTile);
+      downTiles.push(targetTile);
+    }
+    for (let i = Number(y); i <= 10; i += 1) {
+      // console.log('up', x, i);
+      const targetTile = document.querySelector(`.js-data-xy-${x}-${i}`);
+      upTiles.push(targetTile);
+    }
+    for (let i = Number(x); i > 0; i -= 1) {
+      // console.log('left', i, y);
+      const targetTile = document.querySelector(`.js-data-xy-${i}-${y}`);
+      leftTiles.push(targetTile);
+    }
+    for (let i = Number(y); i > 0; i -= 1) {
+      // console.log('right', x, i);
+      const targetTile = document.querySelector(`.js-data-xy-${x}-${i}`);
+      rightTiles.push(targetTile);
+    }
+  }
+  for (let i = 0; i < 10; i += 1) {
+    if (downTiles[i]) {
+      change(downTiles[i]);
+    }
+    if (upTiles[i]) {
+      change(upTiles[i]);
+    }
+    if (leftTiles[i]) {
+      change(leftTiles[i]);
+    }
+    if (rightTiles[i]) {
+      change(rightTiles[i]);
     }
   }
 };
